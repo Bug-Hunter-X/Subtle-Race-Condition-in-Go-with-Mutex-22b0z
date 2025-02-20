12e56266dc0a7767507396c92c176fa666c47d05
BugHunter-X <askzenai@gmail.com>
2025-02-20 20:34:07 +0100

    Add bug.go file

---
+++ bug.go
@@ -0,0 +1,28 @@
+```go
+package main
+
+import (
+
+        "fmt"
+        "sync"
+)
+
+func main() {
+        var wg sync.WaitGroup
+        var m sync.Mutex
+        var x int
+
+        for i := 0; i < 1000; i++ {
+                wg.Add(1)
+                go func(i int) {
+                        defer wg.Done()
+                        m.Lock()
+                        x += i
+                        m.Unlock()
+                }(i)
+        }
+
+        wg.Wait()
+        fmt.Println("x:", x)
+}
+```
